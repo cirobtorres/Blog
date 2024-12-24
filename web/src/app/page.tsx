dbbf8dfa99210be7c7d3045ebd46e9aa2b61cd27
cirobt <cbtorres89@gmail.com>
2024-12-24 15:10:10 -0300

    CKEditor5 configuration

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,3 +1,9 @@
+import dynamic from "next/dynamic";
+
+const BlogEditor = dynamic(() => import("@/components/BlogEditor"), {
+  ssr: true,
+});
+
 export default function HomePage() {
-  return <div></div>;
+  return <BlogEditor />;
 }
---
+++ web/src/components/BlogEditor.tsx
@@ -0,0 +1,382 @@
+"use client";
+
+import { useState, useEffect, useRef, useMemo } from "react";
+import { CKEditor, useCKEditorCloud } from "@ckeditor/ckeditor5-react";
+import { EditorConfig } from "@ckeditor/ckeditor5-core/src/editor/editorconfig";
+import "../styles/ckeditor.css";
+
+export default function BLogEditor() {
+  const editorContainerRef = useRef<HTMLDivElement | null>(null);
+  const editorRef = useRef<HTMLDivElement | null>(null);
+  const editorWordCountRef = useRef<HTMLDivElement | null>(null);
+  const [isLayoutReady, setIsLayoutReady] = useState(false);
+  const [outputData, setOutputData] = useState(""); // ---------> Supabase
+  const cloud = useCKEditorCloud({ version: "44.1.0" });
+
+  useEffect(() => {
+    setIsLayoutReady(true);
+    return () => setIsLayoutReady(false);
+  }, []);
+
+  const handleSave = () => {
+    console.log(outputData); // ---------> Supabase
+  };
+
+  const { ClassicEditor, editorConfig } = useMemo(() => {
+    if (cloud.status !== "success" || !isLayoutReady) {
+      return {};
+    }
+
+    const {
+      ClassicEditor,
+      Alignment,
+      Autoformat,
+      AutoImage,
+      AutoLink,
+      Autosave,
+      BalloonToolbar,
+      Base64UploadAdapter,
+      BlockQuote,
+      BlockToolbar,
+      Bold,
+      Bookmark,
+      CodeBlock,
+      Essentials,
+      FindAndReplace,
+      FontBackgroundColor,
+      FontColor,
+      FullPage,
+      GeneralHtmlSupport,
+      Heading,
+      HeadingButtonsUI,
+      HorizontalLine,
+      HtmlComment,
+      HtmlEmbed,
+      ImageBlock,
+      ImageCaption,
+      ImageInline,
+      ImageInsert,
+      ImageInsertViaUrl,
+      ImageResize,
+      ImageStyle,
+      ImageTextAlternative,
+      ImageToolbar,
+      ImageUpload,
+      Indent,
+      IndentBlock,
+      Italic,
+      Link,
+      LinkImage,
+      List,
+      ListProperties,
+      Markdown,
+      MediaEmbed,
+      ParagraphButtonUI,
+      PasteFromMarkdownExperimental,
+      PasteFromOffice,
+      RemoveFormat,
+      ShowBlocks,
+      SourceEditing,
+      SpecialCharacters,
+      SpecialCharactersArrows,
+      SpecialCharactersCurrency,
+      SpecialCharactersEssentials,
+      SpecialCharactersLatin,
+      SpecialCharactersMathematical,
+      SpecialCharactersText,
+      Strikethrough,
+      Subscript,
+      Superscript,
+      Table,
+      TableCaption,
+      TableCellProperties,
+      TableColumnResize,
+      TableProperties,
+      TableToolbar,
+      TextTransformation,
+      TodoList,
+      WordCount,
+    } = cloud.CKEditor;
+
+    return {
+      ClassicEditor,
+      editorConfig: {
+        licenseKey: process.env.NEXT_PUBLIC_CKEDITOR_LICENSE_KEY,
+        toolbar: {
+          items: [
+            "sourceEditing",
+            "showBlocks",
+            "findAndReplace",
+            "|",
+            "paragraph",
+            "heading1",
+            "heading2",
+            "heading3",
+            "|",
+            "fontColor",
+            "fontBackgroundColor",
+            "|",
+            "bold",
+            "italic",
+            "strikethrough",
+            "subscript",
+            "superscript",
+            "removeFormat",
+            "|",
+            "specialCharacters",
+            "horizontalLine",
+            "link",
+            "bookmark",
+            "|",
+            "uploadImage",
+            "insertImageViaUrl",
+            "mediaEmbed",
+            "|",
+            "insertTable",
+            "blockQuote",
+            "codeBlock",
+            "htmlEmbed",
+            "|",
+            "alignment:left",
+            "alignment:right",
+            "alignment:center",
+            "alignment:justify",
+            "|",
+            "bulletedList",
+            "numberedList",
+            "todoList",
+            "outdent",
+            "indent",
+          ],
+          shouldNotGroupWhenFull: true,
+        },
+        plugins: [
+          Alignment,
+          Autoformat,
+          AutoImage,
+          AutoLink,
+          Autosave,
+          BalloonToolbar,
+          Base64UploadAdapter,
+          BlockQuote,
+          BlockToolbar,
+          Bold,
+          Bookmark,
+          CodeBlock,
+          Essentials,
+          FindAndReplace,
+          FontBackgroundColor,
+          FontColor,
+          FullPage,
+          GeneralHtmlSupport,
+          Heading,
+          HeadingButtonsUI,
+          HorizontalLine,
+          HtmlComment,
+          HtmlEmbed,
+          ImageBlock,
+          ImageCaption,
+          ImageInline,
+          ImageInsert,
+          ImageInsertViaUrl,
+          ImageResize,
+          ImageStyle,
+          ImageTextAlternative,
+          ImageToolbar,
+          ImageUpload,
+          Indent,
+          IndentBlock,
+          Italic,
+          Link,
+          LinkImage,
+          List,
+          ListProperties,
+          Markdown,
+          MediaEmbed,
+          ParagraphButtonUI,
+          PasteFromMarkdownExperimental,
+          PasteFromOffice,
+          RemoveFormat,
+          ShowBlocks,
+          SourceEditing,
+          SpecialCharacters,
+          SpecialCharactersArrows,
+          SpecialCharactersCurrency,
+          SpecialCharactersEssentials,
+          SpecialCharactersLatin,
+          SpecialCharactersMathematical,
+          SpecialCharactersText,
+          Strikethrough,
+          Subscript,
+          Superscript,
+          Table,
+          TableCaption,
+          TableCellProperties,
+          TableColumnResize,
+          TableProperties,
+          TableToolbar,
+          TextTransformation,
+          TodoList,
+          WordCount,
+        ],
+        balloonToolbar: [
+          "paragraph",
+          "heading1",
+          "heading2",
+          "heading3",
+          "|",
+          "bold",
+          "italic",
+          "|",
+          "link",
+          "|",
+          "bulletedList",
+          "numberedList",
+        ],
+        blockToolbar: [
+          "paragraph",
+          "heading1",
+          "heading2",
+          "heading3",
+          "|",
+          "fontColor",
+          "fontBackgroundColor",
+          "|",
+          "bold",
+          "italic",
+          "|",
+          "link",
+          "insertTable",
+          "|",
+          "bulletedList",
+          "numberedList",
+          "outdent",
+          "indent",
+        ],
+        heading: {
+          options: [
+            {
+              model: "paragraph",
+              title: "Paragraph",
+              class: "ck-heading_paragraph",
+            },
+            {
+              model: "heading1",
+              view: "h1",
+              title: "Heading 1",
+              class: "ck-heading_heading1",
+            },
+            {
+              model: "heading2",
+              view: "h2",
+              title: "Heading 2",
+              class: "ck-heading_heading2",
+            },
+            {
+              model: "heading3",
+              view: "h3",
+              title: "Heading 3",
+              class: "ck-heading_heading3",
+            },
+          ],
+        },
+        htmlSupport: {
+          allow: [
+            {
+              name: /^.*$/,
+              styles: true,
+              attributes: true,
+              classes: true,
+            },
+          ],
+        },
+        image: {
+          toolbar: [
+            "toggleImageCaption",
+            "imageTextAlternative",
+            "|",
+            "imageStyle:inline",
+            "imageStyle:wrapText",
+            "imageStyle:breakText",
+            "|",
+            "resizeImage",
+          ],
+        },
+        initialData: "",
+        link: {
+          addTargetToExternalLinks: true,
+          defaultProtocol: "https://",
+          decorators: {
+            toggleDownloadable: {
+              mode: "manual",
+              label: "Downloadable",
+              attributes: {
+                download: "file",
+              },
+            },
+          },
+        },
+        list: {
+          properties: {
+            styles: true,
+            startIndex: true,
+            reversed: true,
+          },
+        },
+        placeholder: "",
+        table: {
+          contentToolbar: [
+            "tableColumn",
+            "tableRow",
+            "mergeTableCells",
+            "tableProperties",
+            "tableCellProperties",
+          ],
+        },
+      },
+    };
+  }, [cloud, isLayoutReady]);
+
+  return (
+    <form className="main-container">
+      <div
+        className="editor-container editor-container_classic-editor editor-container_include-style editor-container_include-block-toolbar editor-container_include-word-count"
+        ref={editorContainerRef}
+      >
+        <div className="editor-container__editor">
+          <div ref={editorRef}>
+            {ClassicEditor && editorConfig && (
+              <CKEditor
+                onReady={(editor) => {
+                  const wordCount = editor.plugins.get("WordCount");
+                  editorWordCountRef.current?.appendChild(
+                    wordCount.wordCountContainer
+                  );
+                }}
+                onChange={(event, editor) => setOutputData(editor.getData())} // ---------> Supabase
+                onAfterDestroy={() => {
+                  if (editorWordCountRef.current)
+                    Array.from(editorWordCountRef.current.children).forEach(
+                      (child) => child.remove()
+                    );
+                }}
+                editor={ClassicEditor}
+                config={editorConfig as EditorConfig}
+              />
+            )}
+          </div>
+        </div>
+        <div
+          className="editor_container__word-count"
+          ref={editorWordCountRef}
+        />
+      </div>
+      <button
+        type="button"
+        onClick={handleSave} // ---------> Supabase
+      >
+        Save
+      </button>
+    </form>
+  );
+}
