7ea5815d03921967f8d75dc4979e895c4b90d7c9
cirobt <cbtorres89@gmail.com>
2025-01-11 16:21:59 -0300

    Home page layout and DarkModeToggle update

--- web/src/components/DarkModeToggle/index.tsx
+++ web/src/components/DarkModeToggle/index.tsx
@@ -1,15 +1,7 @@
 "use client";
 
-import { BsSunFill } from "react-icons/bs";
-import { BsMoonFill } from "react-icons/bs";
+import { IoMoon, IoSunny } from "react-icons/io5";
 import { useTheme } from "next-themes";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "../shadcnui/dropDownMenu";
-import { Button } from "../shadcnui/buttons";
 import {
   Tooltip,
   TooltipContent,
@@ -23,28 +15,27 @@ export default function DarkModeToggle() {
   return (
     <TooltipProvider>
       <Tooltip>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <TooltipTrigger asChild>
-              <Button variant="outline" size="icon">
-                <BsSunFill className="size-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                <BsMoonFill className="absolute size-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-                <span className="sr-only">Botão de tema claro ou escuro</span>
-              </Button>
-            </TooltipTrigger>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuItem onClick={() => setTheme("light")}>
-              Claro
-            </DropdownMenuItem>
-            <DropdownMenuItem onClick={() => setTheme("dark")}>
-              Escuro
-            </DropdownMenuItem>
-            <DropdownMenuItem onClick={() => setTheme("system")}>
-              Sistema
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <TooltipTrigger asChild>
+          <div className="relative">
+            <div className="flex shrink-0 justify-center items-center rounded-full size-8 duration-700 cursor-pointer bg-blog-background-1">
+              <button
+                type="button"
+                onClick={() => setTheme("light")}
+                className="absolute transition-all duration-[600ms] opacity-0 scale-0 dark:opacity-100 dark:scale-100"
+              >
+                <IoMoon className="text-xl text-[#40b88c]" />
+              </button>
+              <button
+                type="button"
+                onClick={() => setTheme("dark")}
+                className="absolute transition-all duration-[600ms] opacity-100 scale-100 dark:opacity-0 dark:scale-0
+                "
+              >
+                <IoSunny className="text-xl text-[#fbbf24]" />
+              </button>
+            </div>
+          </div>
+        </TooltipTrigger>
         <TooltipContent>
           <p>Alternar tema</p>
         </TooltipContent>
--- web/src/lib/articles.ts
+++ web/src/lib/articles.ts
@@ -1,10 +1,13 @@
 const getArticles = async () => {
-  const res = await fetch("http://127.0.0.1:1337/api/articles?populate=cover", {
-    method: "GET",
-    headers: {
-      Authorization: `Bearer ${process.env.NEXT_PUBLIC_STRAPI_TOKEN}`,
-    },
-  });
+  const res = await fetch(
+    "http://127.0.0.1:1337/api/articles?populate=cover&sort=createdAt:desc",
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_STRAPI_TOKEN}`,
+      },
+    }
+  );
   if (!res.ok) {
     console.error(`${res.status} ${res.statusText}`);
     throw new Error("Failed to fetch articles");
